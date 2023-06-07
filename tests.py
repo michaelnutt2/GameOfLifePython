6c5b5f58bbd997647f96f94cb1cfe1038eb4b588
Michael Nutt <michaeltnutt@gmail.com>
2023-06-07 17:16:32 -0500

    Refactored Tests for Class
    Set up the test to take into account the class that is being created for
    the universe

--- tests.py
+++ tests.py
@@ -5,16 +5,20 @@ import life
 
 class TestUniverse(unittest.TestCase):
     def setUp(self) -> None:
-        self.universe = [
+        self.universe_test = [
             0, 0, 0, 0, 0,
             0, 0, 1, 0, 0,
             0, 0, 1, 0, 0,
             0, 0, 1, 0, 0,
             0, 0, 0, 0, 0
         ]
+        self.universe = life.Universe(135296, 5, 5)
 
     def test_initial_universe(self):
-        self.assertEqual(self.universe, life.initial_universe(135296))
+        self.assertEqual(self.universe_test, self.universe.universe)
+
+    def test_check_neighbors(self):
+        ...
 
 
 if __name__ == '__main__':
