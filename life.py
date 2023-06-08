1f0fe4fe830aabc8abff1fb2d23ad7679e11e3fc
Michael Nutt <michaeltnutt@gmail.com>
2023-06-08 12:46:27 -0500

    Code working
    Prints alive cells with # and dead cells with _
    Prints a message at the end when all cells have died.

--- life.py
+++ life.py
@@ -1,3 +1,7 @@
+import os
+import random
+import time
+
 # Constants
 X = 0
 Y = 1
@@ -8,11 +12,11 @@ ALIVE = 1
 DEAD = 0
 NEIGHBORS = (-1, 0, 1)
 UNIVERSE = []
-DEBUG_SEED = 135296
+DEBUG_SEED = 392949192399124912499591827432879238798273947502983097286376587293843453
 
 
 class Universe:
-    def __init__(self, seed, width, height):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
         self.size = self.height * self.width
@@ -20,8 +24,7 @@ class Universe:
         self.update = []
 
         for i in range(self.size):
-            self.universe.append(seed % 2)
-            seed = int(seed / 2)
+            self.universe.append(random.randrange(2))
 
     def __str__(self):
         """
@@ -33,17 +36,28 @@ class Universe:
             if i % self.width == 0:
                 output += "\n"
 
-            output += f'{self.universe[i]} '
+            if self.universe[i] == ALIVE:
+                output += '# '
+            else:
+                output += '_ '
 
         return output
 
     def determine_universe_state(self):
         self.update.clear()
         for i in range(self.size):
-            if self.check_neighbors(i):
+            if self.check_neighbors(i) != self.universe[i]:
                 self.update.append(i)
+        self.update_cells()
 
     def check_neighbors(self, index) -> int:
+        """
+        Checks the number of neighbors that are alive
+
+        :param index: Cell we are checking neighbors of
+        :return: 0 or 1, result for if the cell should be alive or dead after
+        the neighbor check.
+        """
         is_alive = False
         if self.universe[index] == ALIVE:
             is_alive = True
@@ -69,7 +83,12 @@ class Universe:
         return DEAD
 
     def update_cells(self):
-        ...
+        for i in self.update:
+            state = self.universe[i]
+            if state == ALIVE:
+                self.universe[i] = DEAD
+            else:
+                self.universe[i] = ALIVE
 
     def x_y_conv(self, index) -> (int, int):
         x = index % self.width
@@ -82,5 +101,13 @@ class Universe:
 
 
 if __name__ == "__main__":
-    universe = Universe(DEBUG_SEED, 5, 5)
-    print(universe)
+    universe = Universe(40, 40)
+    while True:
+        if 1 not in universe.universe:
+            os.system("clear")
+            print("The universe has died!")
+            exit()
+        os.system("clear")
+        print(universe)
+        universe.determine_universe_state()
+        time.sleep(1)
--- tests.py
+++ tests.py
@@ -12,13 +12,8 @@ class TestUniverse(unittest.TestCase):
             0, 0, 1, 0, 0,
             0, 0, 0, 0, 0
         ]
-        self.universe = life.Universe(135296, 5, 5)
-
-    def test_initial_universe(self):
-        self.assertEqual(self.universe_test, self.universe.universe)
-
-    def test_check_neighbors(self):
-        ...
+        self.universe = life.Universe(5, 5)
+        self.universe.universe = self.universe_test
 
     def test_x_y_conv(self):
         self.assertEqual((4, 4), self.universe.x_y_conv(24))
@@ -37,6 +32,11 @@ class TestUniverse(unittest.TestCase):
 
     def test_check_neighbors_start_dead_end_dead(self):
         self.assertEqual(0, self.universe.check_neighbors(0))
+
+    def test_update_cells(self):
+        self.universe.update = [0]
+        self.universe.update_cells()
+        self.assertEqual(1, self.universe.universe[0])
 
 
 if __name__ == '__main__':
